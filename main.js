b6f1d2ff58d1709db454c85c63119f0214ff1960
Javohir <johndoe@example.com>
2024-06-06 23:47:58 +0500

    Isimlar

---
+++ main.js
@@ -0,0 +1,18 @@
+// Funksiya fanction
+
+function hello (name, bgcolor,color){
+    document.write(`<p style="background-color: ${bgcolor}; color: ${color};">${name}</p>`);
+}
+
+hello('Islom','yellow','red');
+hello('Javohir','aqua','red');
+hello('Diyorbek','blue','white');
+hello('Abdurahmon','green','white');
+hello('Alikoder','gray','white');
+
+
+
+
+// hello('Islom');
+// hello('Javohir');
+// hello();
